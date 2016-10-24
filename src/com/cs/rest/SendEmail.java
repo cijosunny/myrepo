bb2e09689891b7ce078af034a906766726b24d64
Cijo <cijosunny@gmail.com>
2016-10-24 21:00:13 +0530

    Changes

--- src/com/cs/rest/SendEmail.java
+++ src/com/cs/rest/SendEmail.java
@@ -12,37 +12,39 @@ import javax.mail.internet.MimeMessage;
 
 public class SendEmail {
 	public static void sendEmail() {
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
 
-		Session session = Session.getInstance(props,
-			new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("testemail4hackathon@gmail.com","WalmartCareers");
-				}
-			});
+		 String to="cijosunny@gmail.com";//change accordingly
 
-		try {
+		//Get the session object
+		  Properties props = new Properties();
+		  props.put("mail.smtp.host", "smtp.gmail.com");
+		  props.put("mail.smtp.socketFactory.port", "465");
+		  props.put("mail.smtp.socketFactory.class",
+		        	"javax.net.ssl.SSLSocketFactory");
+		  props.put("mail.smtp.auth", "true");
+		  props.put("mail.smtp.port", "465");
+		 
+		  Session session = Session.getDefaultInstance(props,
+		   new javax.mail.Authenticator() {
+		   protected PasswordAuthentication getPasswordAuthentication() {
+		   return new PasswordAuthentication("testemail4hackathon@gmail.com","WalmartCareers");//change accordingly
+		   }
+		  });
+		 
+		//compose message
+		  try {
+		   MimeMessage message = new MimeMessage(session);
+		   message.setFrom(new InternetAddress("testemail4hackathon@gmail.com"));//change accordingly
+		   message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+		   message.setSubject("Hello");
+		   message.setText("Testing.......");
+		   
+		   //send message
+		   Transport.send(message);
 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("testemail4hackathon@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("deathisonce@gmail.com"));
-			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler," +
-					"\n\n No spam to my email, please!");
-
-			Transport.send(message);
-
-			System.out.println("Done");
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+		   System.out.println("message sent successfully");
+		 
+		  } catch (MessagingException e) {throw new RuntimeException(e);}
+		 
+		 }
 }
